ae13ef0c960476de74b7d27a496adf4675940b8d
DennisGL1 <dennis@gamelevelone.com>
2017-12-13 18:05:57 +0700

    - modified soldier vision trigger - add tag "Openable"

--- Assets/Scripts/Soldier/SoldierVisionTriggerCollider.cs
+++ Assets/Scripts/Soldier/SoldierVisionTriggerCollider.cs
@@ -1,20 +1,31 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class SoldierVisionTriggerCollider : SoldierBaseTriggerCollider {
+public class SoldierVisionTriggerCollider : MonoBehaviour {
 	[Header("VisionTriggerCollider Attributes")]
+	public Soldier soldier;
 	public string niwelTag;
 	public string monsterTag;
 	public bool longVision;
 
-	protected override void OnTriggerEnter2D(Collider2D other)
+	[Header("Do Not Modify")]
+	public List<GameObject> hidingPlace = new List<GameObject>();
+
+	protected void OnTriggerEnter2D(Collider2D other)
 	{
+		if(!longVision && other.tag == Tags.HIDEABLE){
+			hidingPlace.Add(other.gameObject);
+		}
+
 		Trigger(other.tag);
 	}
 
 	protected void OnTriggerExit2D(Collider2D other)
 	{
-
+		if(!longVision && other.tag == Tags.HIDEABLE){
+			hidingPlace.Remove(other.gameObject);
+		}
 	}
 
 	protected void Trigger (string otherTag)
